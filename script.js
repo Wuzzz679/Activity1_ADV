4999837a8541579d4e23843f19df64b50d485715
Wuzzz679 <russel.ebanez@hcdc.edu.ph>
2025-08-14 18:37:05 +0800

    Initial commit

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-
+// 1. 
 var name = "Russel Ebañez";
 console.log(name);
 
+// 2. 
 let age = "19";
 console.log(age);
-
+// 3. 
 const PI = 3.1416;
-PI = 3.14; 
+// PI = 3.14; //gi comment nako sir para di mag error
+console.log(PI);
+
+// 4. 
+let isStudent = Number(age) < 25;
+console.log(isStudent);
+
+// 5. 
+let person = {
+    firstName: "Russel",
+    lastName: "Ebañez",
+    address: {
+        barangay: "Maa",
+        city: "Davao City",
+        province:"Davao del Sur"
+    }
+};
+console.log(person.firstName + " " + person.lastName);
+console.log(person.address.barangay + ", " + person.address.city + ", " + person.address.province);
+
+// 6.
+let colors = ["red", "blue", "green", "yellow", "blue"];
+
+
+
+
+
